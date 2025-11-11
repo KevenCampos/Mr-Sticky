2beddb010985d649a243333c90c1d4deaeaff540
Keven <kevencampos1@hotmail.com>
2025-11-11 15:40:46 -0300

    first commit

---
+++ bot/index.ts
@@ -0,0 +1,8 @@
+import { Client } from "fast-discord-js";
+
+const client = new Client({ autoImport: ["bot/commands", "bot/handler"]});
+client.login(process.env.BOT_TOKEN!)
+
+client.on("clientReady", (client) => {
+    console.log(`Bot ${client?.user.username} connected`)
+})
