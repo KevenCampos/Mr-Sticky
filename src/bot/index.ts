e7ff1118db741a11061cb2498ae40a0506aed382
KevenCampos <kevencampos1@hotmail.com>
2025-11-14 01:21:44 -0300

    ok

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -1,12 +1,21 @@
 import { Client } from "fast-discord-js";
+import { Client as DiscordClient } from "discord.js";
 
 const client = new Client({ autoImport: ["src/bot/commands", "src/bot/handler"]});
 client.login(process.env.BOT_TOKEN!)
 
 client.on("clientReady", (client) => {
     console.log(`Bot ${client?.user.username} (${client?.user.id}) connected`)
+    updateActivity(client);
 
-    client.user.setActivity({ name: "Best Stickies", type: 4 });
+    setInterval(() => {
+        updateActivity(client);
+    }, 10000);
 })
+
+
+const updateActivity = (client: DiscordClient) => {
+    client?.user?.setActivity({ name: `Best Stickies #${client?.guilds.cache.size} guilds`, type: 4 });
+}
 
 export { client };
